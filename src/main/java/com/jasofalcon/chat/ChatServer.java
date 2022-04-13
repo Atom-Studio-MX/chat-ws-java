e57aa7caee4bf24bc34008e0cd2a6d57965c7a16
ichi <ichi0915@gmail.com>
2022-04-12 21:48:30 -0500

    Updated dependencies

--- src/main/java/com/jasofalcon/chat/ChatServer.java
+++ src/main/java/com/jasofalcon/chat/ChatServer.java
@@ -20,124 +20,135 @@ import java.util.Set;
 
 public class ChatServer extends WebSocketServer {
 
-    private final static Logger logger = LogManager.getLogger(ChatServer.class);
+	private final static Logger logger = LogManager.getLogger(ChatServer.class);
 
-    private HashMap<WebSocket, User> users;
+	private HashMap<WebSocket, User> users;
 
-    private Set<WebSocket> conns;
+	private Set<WebSocket> conns;
 
-    private ChatServer(int port) {
-        super(new InetSocketAddress(port));
-        conns = new HashSet<>();
-        users = new HashMap<>();
-    }
+	private ChatServer(int port) {
+		super(new InetSocketAddress(port));
+		conns = new HashSet<>();
+		users = new HashMap<>();
 
-    @Override
-    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
-        conns.add(webSocket);
+		logger.info("ChatServer created");
 
-        logger.info("Connection established from: " + webSocket.getRemoteSocketAddress().getHostString());
-        System.out.println("New connection from " + webSocket.getRemoteSocketAddress().getAddress().getHostAddress());
-    }
+		if (logger.isDebugEnabled()) {
+			logger.debug("Debug Mode ON");
+		}
+	}
 
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        conns.remove(conn);
-        // When connection is closed, remove the user.
-        try {
-            removeUser(conn);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void onStart() {
+		logger.info("ChatServer Started");
+	}
 
-        logger.info("Connection closed to: " + conn.getRemoteSocketAddress().getHostString());
-        System.out.println("Closed connection to " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
-    }
+	@Override
+	public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+		conns.add(webSocket);
 
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Message msg = mapper.readValue(message, Message.class);
+		logger.info("Connection established from: " + webSocket.getRemoteSocketAddress().getHostString());
+		System.out.println("New connection from " + webSocket.getRemoteSocketAddress().getAddress().getHostAddress());
+	}
 
-            switch (msg.getType()) {
-                case USER_JOINED:
-                    addUser(new User(msg.getUser().getName()), conn);
-                    break;
-                case USER_LEFT:
-                    removeUser(conn);
-                    break;
-                case TEXT_MESSAGE:
-                    broadcastMessage(msg);
-            }
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+		conns.remove(conn);
+		// When connection is closed, remove the user.
+		try {
+			removeUser(conn);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 
-            System.out.println("Message from user: " + msg.getUser() + ", text: " + msg.getData() + ", type:" + msg.getType());
-            logger.info("Message from user: " + msg.getUser() + ", text: " + msg.getData());
-        } catch (IOException e) {
-            logger.error("Wrong message format.");
-            // return error message to user
-        }
-    }
+		logger.info("Connection closed to: " + conn.getRemoteSocketAddress().getHostString());
+		System.out.println("Closed connection to " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
+	}
 
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Message msg = mapper.readValue(message, Message.class);
 
-        if (conn != null) {
-            conns.remove(conn);
-        }
-        assert conn != null;
-        System.out.println("ERROR from " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
-    }
+			switch (msg.getType()) {
+				case USER_JOINED:
+					addUser(new User(msg.getUser().getName()), conn);
+					break;
+				case USER_LEFT:
+					removeUser(conn);
+					break;
+				case TEXT_MESSAGE:
+					broadcastMessage(msg);
+			}
 
-    private void broadcastMessage(Message msg) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String messageJson = mapper.writeValueAsString(msg);
-            for (WebSocket sock : conns) {
-                sock.send(messageJson);
-            }
-        } catch (JsonProcessingException e) {
-            logger.error("Cannot convert message to json.");
-        }
-    }
+			System.out.println("Message from user: " + msg.getUser() + ", text: " + msg.getData() + ", type:" + msg.getType());
+			logger.info("Message from user: " + msg.getUser() + ", text: " + msg.getData());
+		} catch (IOException e) {
+			logger.error("Wrong message format.");
+			// return error message to user
+		}
+	}
 
-    private void addUser(User user, WebSocket conn) throws JsonProcessingException {
-        users.put(conn, user);
-        acknowledgeUserJoined(user, conn);
-        broadcastUserActivityMessage(MessageType.USER_JOINED);
-    }
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
 
-    private void removeUser(WebSocket conn) throws JsonProcessingException {
-        users.remove(conn);
-        broadcastUserActivityMessage(MessageType.USER_LEFT);
-    }
+		if (conn != null) {
+			conns.remove(conn);
+		}
+		assert conn != null;
+		System.out.println("ERROR from " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
+	}
 
-    private void acknowledgeUserJoined(User user, WebSocket conn) throws JsonProcessingException {
-        Message message = new Message();
-        message.setType(MessageType.USER_JOINED_ACK);
-        message.setUser(user);
-        conn.send(new ObjectMapper().writeValueAsString(message));
-    }
+	private void broadcastMessage(Message msg) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String messageJson = mapper.writeValueAsString(msg);
+			for (WebSocket sock : conns) {
+				sock.send(messageJson);
+			}
+		} catch (JsonProcessingException e) {
+			logger.error("Cannot convert message to json.");
+		}
+	}
 
-    private void broadcastUserActivityMessage(MessageType messageType) throws JsonProcessingException {
+	private void addUser(User user, WebSocket conn) throws JsonProcessingException {
+		users.put(conn, user);
+		acknowledgeUserJoined(user, conn);
+		broadcastUserActivityMessage(MessageType.USER_JOINED);
+	}
 
-        Message newMessage = new Message();
+	private void removeUser(WebSocket conn) throws JsonProcessingException {
+		users.remove(conn);
+		broadcastUserActivityMessage(MessageType.USER_LEFT);
+	}
 
-        ObjectMapper mapper = new ObjectMapper();
-        String data = mapper.writeValueAsString(users.values());
-        newMessage.setData(data);
-        newMessage.setType(messageType);
-        broadcastMessage(newMessage);
-    }
+	private void acknowledgeUserJoined(User user, WebSocket conn) throws JsonProcessingException {
+		Message message = new Message();
+		message.setType(MessageType.USER_JOINED_ACK);
+		message.setUser(user);
+		conn.send(new ObjectMapper().writeValueAsString(message));
+	}
 
-    public static void main(String[] args) {
-        int port;
-        try {
-            port = Integer.parseInt(System.getenv("PORT"));
-        } catch (NumberFormatException nfe) {
-            port = 9000;
-        }
-        new ChatServer(port).start();
-    }
+	private void broadcastUserActivityMessage(MessageType messageType) throws JsonProcessingException {
+
+		Message newMessage = new Message();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String data = mapper.writeValueAsString(users.values());
+		newMessage.setData(data);
+		newMessage.setType(messageType);
+		broadcastMessage(newMessage);
+	}
+
+	public static void main(String[] args) {
+		int port;
+		try {
+			port = Integer.parseInt(System.getenv("PORT"));
+		} catch (NumberFormatException nfe) {
+			port = 9000;
+		}
+		new ChatServer(port).start();
+	}
 
 }
